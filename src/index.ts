9ae118ef9de09ea6f218b57b62886d4839fb3718
Ulisses <ulissessuporteti01@gmail.com>
2023-04-24 14:13:03 -0300

    adicionando e configurando o webpack Getting Started - https://webpack.js.org/guides/getting-started/ TypeScript - https://webpack.js.org/guides/typescript/

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import z from 'zod'
+
+console.log(z);
+
+// const baseURL = "http://localhost:3000/"
+// const conteudo_tabela = document.querySelector('.conteudo tbody')
+// const item = {
+//   nome: "Produto A",
+//   marca: "Marca A",  
+//   qtd: 10
+// }
+
+
+// async function createItem(item: any) {
+//   try {
+//     const response = await fetch(baseURL + "cadastroProdutos", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(item),
+//     });
+//     console.log(response)
+//     return response.json();
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// }
+
+// createItem(item).then(function(response) {
+//   console.log(response)
+// })
+
+
+// async function request() {
+//     try{
+//       const response = await fetch(baseURL + "cadastroProdutos");
+//       return response.json()
+//     }catch(e) {
+//       console.error(e);
+//     };
+// };
+
+// request().then(function(response) {
+//   console.log(response)
+//     response.forEach((produto: any) => {
+//         const tr = document.createElement('tr');
+//         tr.innerHTML = `
+//           <td>${produto.id}</td>
+//           <td>${produto.nome}</td>
+//           <td>${produto.marca}</td>
+//           <td>${produto.qtd}</td>
+//           <td>
+//             <button>editar</button>
+//             <button>remover</button>
+//           </td>
+//         `;
+//         conteudo_tabela!.appendChild(tr);
+//       });
+// });
+
+
+
