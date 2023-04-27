179277c84b99644f825e1540ea1ff1a9108e58b1
Ulisses <ulissessuporteti01@gmail.com>
2023-04-27 16:45:57 -0300

    criando funções para realizar operações no localStorage - set, get and clear

---
+++ src/utils/localStorage/index.ts
@@ -0,0 +1,13 @@
+function setDataLocalStorage<T>(key: string, data: T) {
+	localStorage[key] = JSON.stringify(data);
+	return;
+}
+  
+const getDataLocalStorage = (key: string) => {
+	if (typeof window !== 'undefined' && localStorage[key] !== undefined) return JSON.parse(localStorage[key]);
+};
+
+
+const localStorageClear = ()=> localStorage.clear();
+  
+export { setDataLocalStorage, getDataLocalStorage, localStorageClear };
