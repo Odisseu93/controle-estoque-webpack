f6f83faf9556dfb82334276fbbd24320228b7940
Ulisses <ulissessuporteti01@gmail.com>
2023-05-04 21:55:39 -0300

    adicionando o componente RenderProvider no App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Form from './components/Form';
 import Table from './components/Table';
 import FormContextProvider from './context/form/provider';
+import { RenderProvider } from './context/render/provider';
 
 
 const App = () => {
 return (
     <>
     <FormContextProvider>
+     <RenderProvider>  
     <Table />
     <Form />
+    </RenderProvider> 
     </FormContextProvider>
     </>
 )
