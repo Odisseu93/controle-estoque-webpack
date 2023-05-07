c9fea04da261fb0315c842ac29215500cb5adfc9
Ulisses <ulissessuporteti01@gmail.com>
2023-05-07 19:33:13 -0300

    alterando a "baseURL" do axios-config

--- src/services/axios-config/index.ts
+++ src/services/axios-config/index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+// localmente http://localhost:3333/products/
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333/products/'  
+    baseURL: 'https://mockapi-controle-estoque.onrender.com/products'  
 });
 
 export default api
