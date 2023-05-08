650b083d876d752cc67048c2ffc01138aa3eb7dc
Ulisses <ulissessuporteti01@gmail.com>
2023-05-08 19:02:49 -0300

    adicionando style para App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import './styles/app.scss';
 import Table from './components/Table';
 import Modal from './components/Modal';
 import { Alert } from './components/Alert';
 
 
+
 const App = () => {
     return (
-        <>
+        <div className='app'>
             <Table />
             <Modal />
             <Alert />
-        </>
+        </div>
     )
 };
 
